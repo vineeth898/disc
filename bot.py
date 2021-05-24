dafba5274b1e100cb1fed321920dcbd2fa0c9cfc
vineeth898 <62250463+vineeth898@users.noreply.github.com>
2021-05-24 11:43:39 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+bot=discord.Client()
+chat=bot.get_channel(843116355947331635)
+@bot.event
+async def on_ready():
+    chat=bot.get_channel(843116355947331635)
+    await chat.send("hello! this is testbot")
+@bot.event
+async def on_message(message):
+    if(message.content=="whats your name"):
+        chat=bot.get_channel(843116355947331635)
+        await chat.send("yo adi my name is bot")
+    if(message.content=="YO"):
+        chat=bot.get_channel(843116355947331635)
+        await chat.send("yo man")
+bot.run("ODQ1NTMzNjQ5NDAzMzc5NzEz.YKiWkw.wlw2E5iqOvcD7vBZ-em7Vn6QFJY")
